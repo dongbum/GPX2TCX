f7009361c1e1da8529f834c1e7f9637c29836a08
DONGBUM, KIM <dongbum9@gmail.com>
2018-07-10 23:53:55 +0900

    기본 파일 생성

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from gpx2tcx import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+application = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    print('starting test server...')
+
+    application.run(host='0.0.0.0', port=5000, debug=True)
