8d60f69d0339fe714eb0635085a61c3069ec92de
DONGBUM, KIM <dongbum9@gmail.com>
2018-07-10 23:48:31 +0900

    기본 파일 생성

---
+++ gpx2tcx/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, url_for
+
+def print_settings(config):
+    print('===================================================================')
+    print('settings for gpx2tcx')
+    print('===================================================================')
+    for key, value in config:
+        print('%s=%s' % (key, value))
+    print('===================================================================')
+
+def not_found(error):
+    return render_template('404.html'), 404
+
+def create_app(config_filepath='resource/config.cfg'):
+    gpx2tcx_app = Flask(__name__)
+
+    return gpx2tcx_app
