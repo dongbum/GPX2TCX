fae1feb51553a351c21583df02e983369df25120
DONGBUM, KIM <dongbum9@gmail.com>
2018-07-11 21:22:07 +0900

    업로드 코드 수정

--- gpx2tcx/controller/upload.py
+++ gpx2tcx/controller/upload.py
@@ -1,6 +1,12 @@
 from flask import render_template, request
 from werkzeug.utils import secure_filename
 from gpx2tcx.blueprint import gpx2tcx
+import os, uuid
+
+ALLOWED_EXTENSIONS = set(['gpx'])
+
+def __allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @gpx2tcx.route('/upload')
 def upload():
@@ -8,8 +14,20 @@ def upload():
 
 @gpx2tcx.route('/upload_process', methods=['POST'])
 def upload_process():
-    if request.method == 'POST':
-        f = request.files['gpxfile']
-        f.save('D:/Work/gpx2tcx.git/uploaded/' + secure_filename(f.filename))
 
-    return '파일 업로드 성공'
+    upload_gpx = request.files['gpxfile']
+
+    try:
+        if upload_gpx and __allowed_file(upload_gpx.filename):
+            ext = (upload_gpx.filename).rsplit('.', 1)[1]
+
+            upload_folder = os.path.join(gpx2tcx.root_path, gpx2tcx.config['UPLOAD_FOLDER'])
+            filename = secure_filename(upload_gpx.filename + '_' + str(uuid.uuid4()) + '.' + ext)
+            upload_gpx.save(os.path.join(upload_folder, filename))
+
+        return '파일 업로드 성공'
+
+    except Exception as e:
+        raise e
+
+    return '파일 업로드 실패'
